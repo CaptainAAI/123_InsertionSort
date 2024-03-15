0b6034ccee90a0e0e9d425f2f5c8968ddcd37ba7
CaptainAAI <anandaagungismail@gmail.com>
2024-03-15 11:27:49 +0700

    display procedure

--- InsertionShort/InsertionShort.cpp
+++ InsertionShort/InsertionShort.cpp
@@ -1,20 +1,67 @@
 #include<iostream>
 using namespace std;
 
+int n;
+int arr[11];
+
+
+void input() {
+	while (true) {
+		cout << "Masukkan banyaknya elemen pada array : ";
+		cin >> n;
+		if (n <= 11)
+			break;
+		else {
+			cout << "\nArray dapat mempunyai maksimal 11 elemen.\n";
+		}
+	}
+	cout << endl;
+	cout << "==================================" << endl;
+	cout << "Masukkan Elemen Array" << endl;
+	cout << "==================================" << endl;
+
+	for (int i = 0; i < n; i++) {
+		cout << "Data ke-" << (i + 1) << ": ";
+		cin >> arr[i];
+	}
+
+}
+
 void insertionsort()
 {
-	int i, n, temp, j;
-	int arr[11];
-
+	int i, temp, j;
+	
 	for (i = 1; i <= n - 1; i++) //step 1
 	{
 		temp = arr[i]; //step 2
-		j = 1 - 1; //step 3
-		while (j >= 0 && arr[j] > temp); //step 4
+		j = i - 1; //step 3
+		while (j >= 0 && arr[j] > temp) //step 4
 		{
 			arr[j + 1] = arr[j]; //step 4a
 			j = j - 1; //step 4b
 		}
 		arr[j + 1] = temp; //step 5
 	}
+}
+
+void display() {
+	cout << endl;
+	cout << "==================================" << endl;
+	cout << "Element Arraay yang telah tersusun" << endl;
+	cout << "==================================" << endl;
+	for (int j = 0; j < n; j++) {
+		cout << arr[j] << endl;
+	}
+	cout << "jumlah pass = " << n - 1 << endl;
+	cout << endl;
+}
+
+int main()  
+{
+	input();
+	insertionsort();
+	display();
+	system("pause");
+
+	return 0;
 }
