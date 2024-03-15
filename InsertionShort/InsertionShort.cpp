1f759fcffe9b28c8940463c20e1dc07c57f5e9b2
CaptainAAI <anandaagungismail@gmail.com>
2024-03-15 10:43:43 +0700

    insertion sort void ( procedure )

--- InsertionShort/InsertionShort.cpp
+++ InsertionShort/InsertionShort.cpp
@@ -5,4 +5,16 @@ void insertionsort()
 {
 	int i, n, temp, j;
 	int arr[11];
+
+	for (i = 1; i <= n - 1; i++) //step 1
+	{
+		temp = arr[i]; //step 2
+		j = 1 - 1; //step 3
+		while (j >= 0 && arr[j] > temp); //step 4
+		{
+			arr[j + 1] = arr[j]; //step 4a
+			j = j - 1; //step 4b
+		}
+		arr[j + 1] = temp; //step 5
+	}
 }
